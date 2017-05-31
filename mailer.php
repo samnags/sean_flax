f52ab8e661dd6db5644713f23e6d35728adbb1cd
samnags <sam.nagourney@gmail.com>
2017-05-31 00:02:21 -0700

    first draft

---
+++ mailer.php
@@ -0,0 +1,35 @@
+<?php
+
+    // Get the form fields, removes html tags and whitespace.
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+     $message = trim($_POST["message"]);
+
+    // Check the data.
+    if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: http://benjaminkushner.com/test.php?success=-1#form");
+        exit;
+    }
+
+    // Set the recipient email address. Update this to YOUR desired email address.
+    $recipient = "sam.nagourney@gmail..com";
+
+    // Set the email subject.
+    $subject = "Email from Site from $name";
+
+    // Build the email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers.
+    $email_headers = "From: $name <$email>";
+
+    // Send the email.
+    mail($recipient, $subject, $email_content, $email_headers);
+    
+    // Redirect to the index.html page with success code
+    header("Location: http://benjaminkushner.com/test.php?success=1#form");
+
+?>

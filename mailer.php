9bd81fe8ad45e27a211fc9651ec098f7142d5096
samnags <sam.nagourney@gmail.com>
2017-06-15 09:14:29 -0400

    responsive

--- mailer.php
+++ mailer.php
@@ -8,12 +8,12 @@
 
     // Check the data.
     if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: https://samnags.github.io/sean_flax/index.php?success=-1#form");
+        header("Location: http://seanflax.com/index.php?success=-1#form");
         exit;
     }
 
     // Set the recipient email address. Update this to YOUR desired email address.
-    $recipient = "sam.nagourney@gmail..com";
+    $recipient = "info@endlessroots.tv";
 
     // Set the email subject.
     $subject = "Email from Site from $name";
@@ -30,6 +30,6 @@
     mail($recipient, $subject, $email_content, $email_headers);
     
     // Redirect to the index.html page with success code
-    header("Location: https://samnags.github.io/sean_flax/index.php?success=1#form");
+    header("Location: http://seanflax.com/index.php?success=1#form");
 
 ?>
